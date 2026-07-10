26e5e1b8bb5bfddc489ea044b6e2947e2a4f96a0
Nguyen Thanh Tung <tung.nguyenthanh5@gameloft.com>
2026-07-10 17:51:23 +0700

    - update info in config file

--- ScriptsBuildOnMac/0-config.sh
+++ ScriptsBuildOnMac/0-config.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+# All these paths are on the Mac
+
 PROJECT="/Users/admin/Documents/GitHub/kinder"
 BUILD_SCRIPT_FOLDER="/Users/admin/Documents/GitHub/BuildIOSFromWindows"
-
-BUILD_ROOT="$PROJECT/_build"
-export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
-
 UNITY="/Applications/Unity/Hub/Editor/6000.3.12f1/Unity.app/Contents/MacOS/Unity"
+# Windows shared folder mounted on the Mac
+WINDOW_SHARE="/Volumes/Shared_Write/IOS_auto_build"
 
+
+
+
+# Default Path
+export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
+BUILD_ROOT="$PROJECT/_build"
 PROJECT_PATH="$BUILD_ROOT/ios"
 ARCHIVE_PATH="$BUILD_ROOT/Archive.xcarchive"
 EXPORT_PATH="$BUILD_ROOT/IPA"
-EXPORT_OPTIONS="/Users/admin/Documents/GitHub/BuildIOSFromWindows/ScriptsBuildOnMac/ExportOptions.plist"
-
-# Windows shared folder
-WINDOW_SHARE="/Volumes/Shared_Write/IOS_auto_build"
+EXPORT_OPTIONS="$BUILD_SCRIPT_FOLDER/ScriptsBuildOnMac/ExportOptions.plist"
 
 SCHEME="Unity-iPhone"
 CONFIGURATION="Release"
+
+
