4f5670706123c5c35905c3222bbaea2f79c011fb
Nguyen Thanh Tung <tung.nguyenthanh5@gameloft.com>
2026-07-13 15:08:45 +0700

    - no need save log for update script

--- update-build-scripts.bat
+++ update-build-scripts.bat
@@ -3,28 +3,6 @@
 set MAC_AUTO_BUILD=Users/admin/Documents/GitHub/BuildIOSFromWindows
 
 echo ----- Window - update-build-scripts
-echo.
-
-ssh admin@10.219.12.174 "/%MAC_AUTO_BUILD%/ScriptsBuildOnMac/update-build-scripts.sh" > build.log 2>&1
-
-set EXIT_CODE=%ERRORLEVEL%
-
-if %EXIT_CODE% neq 0 (
-    echo.
-    echo ========================================
-    echo        BUILD FAILED!
-    echo.
-    echo Exit Code: %EXIT_CODE%
-    echo Check build.log for details.
-    echo ========================================
-    pause
-    exit /b %EXIT_CODE%
-)
-
-echo.
-echo ========================================
-echo        BUILD SUCCESS!
-echo ========================================
+ssh admin@10.219.12.174 "/%MAC_AUTO_BUILD%/ScriptsBuildOnMac/update-build-scripts.sh"
 
 pause
-exit /b 0
